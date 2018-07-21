dbad3c8c936c8704da7d55ade43f1c90a0829eb8
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-07-21 22:34:54 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,116 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { AlertController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pairedList: pairedlist;
+  listToggle: boolean = false;
+  pairedDeviceID: number = 0;
+  dataSend: string = "";
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private bluetoothSerial: BluetoothSerial, private toastCtrl: ToastController) {
+    this.checkBluetoothEnabled();
+  }
+
+  checkBluetoothEnabled() {
+    this.bluetoothSerial.isEnabled().then(success => {
+      this.listPairedDevices();
+    }, error => {
+      this.showError("Please Enable Bluetooth")
+    });
+  }
+
+  listPairedDevices() {
+    this.bluetoothSerial.list().then(success => {
+      this.pairedList = success;
+      this.listToggle = true;
+    }, error => {
+      this.showError("Please Enable Bluetooth")
+      this.listToggle = false;
+    });
+  }
+
+  selectDevice() {
+    let connectedDevice = this.pairedList[this.pairedDeviceID];
+    if (!connectedDevice.address) {
+      this.showError('Select Paired Device to connect');
+      return;
+    }
+    let address = connectedDevice.address;
+    let name = connectedDevice.name;
+
+    this.connect(address);
+  }
+
+  connect(address) {
+    // Attempt to connect device with specified address, call app.deviceConnected if success
+    this.bluetoothSerial.connect(address).subscribe(success => {
+      this.deviceConnected();
+      this.showToast("Successfully Connected");
+    }, error => {
+      this.showError("Error:Connecting to Device");
+    });
+  }
+
+  deviceConnected() {
+    // Subscribe to data receiving as soon as the delimiter is read
+    this.bluetoothSerial.subscribe('\n').subscribe(success => {
+      this.handleData(success);
+      this.showToast("Connected Successfullly");
+    }, error => {
+      this.showError(error);
+    });
+  }
+
+  deviceDisconnected() {
+    // Unsubscribe from data receiving
+    this.bluetoothSerial.disconnect();
+    this.showToast("Device Disconnected");
+  }
+
+  handleData(data) {
+    this.showToast(data);
+  }
+
+  sendData() {
+    this.dataSend+='\n';
+    this.showToast(this.dataSend);
+
+    this.bluetoothSerial.write(this.dataSend).then(success => {
+      this.showToast(success);
+    }, error => {
+      this.showError(error)
+    });
+  }
+
+  showError(error) {
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: error,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  showToast(msj) {
+    const toast = this.toastCtrl.create({
+      message: msj,
+      duration: 1000
+    });
+    toast.present();
 
   }
 
+}
+
+interface pairedlist {
+  "class": number,
+  "id": string,
+  "address": string,
+  "name": string
 }
